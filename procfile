d77ebf1663f058344b0cfb564a2536b469651986
Harsh Vats <vatsvatsharsh2000@gmail.com>
2020-10-16 00:40:43 +0530

    ignore

--- procfile
+++ procfile
@@ -1,1 +1,1 @@
-web: node backend/index.js
+web: node server.js
--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(cors());
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 } else {
   console.log('oops');
