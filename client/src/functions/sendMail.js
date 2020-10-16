07f54a28c526a1457de712d084f6453334536c17
Harsh Vats <vatsvatsharsh2000@gmail.com>
2020-10-16 22:25:56 +0530

    fixed invite function

--- client/src/functions/sendMail.js
+++ client/src/functions/sendMail.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import { baseurl } from '../constants';
 
 export const sendMail = async (email, name) => {
   try {
-    const res = await axios.post(`${baseurl()}/api/sendmail/${email}/${name}`);
+    const res = await axios.post(`/sendmail/${email}/${name}`);
   } catch (error) {
     console.log('error occurred', error);
   }
