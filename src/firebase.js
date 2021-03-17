743de37b50470f5e1dc6b349e7d019b308d118cd
Harsh Vats <vatsvatsharsh2000@gmail.com>
2021-03-17 19:26:23 +0530

    firebase api key is now in enviornment variable

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,16 @@
 import * as firebase from "firebase";
 
+console.log(process.env);
+
 let config = {
-  apiKey: "AIzaSyCf-4xmiIRxKUgUY3YVWtSEeZIuVhtiwXM",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "newsgamer-e328f.firebaseapp.com",
   databaseURL: "https://newsgamer-e328f.firebaseio.com",
   projectId: "newsgamer-e328f",
   storageBucket: "newsgamer-e328f.appspot.com",
   messagingSenderId: "934359092726",
   appId: "1:934359092726:web:b11971d68875933ebb0bd7",
-  measurementId: "G-F1CTM60RR7",
+  measurementId: "G-F1CTM60RR7"
 };
 firebase.initializeApp(config);
 firebase.analytics();
